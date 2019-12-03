0067178e5ed4b40d3615fd6dd261face537741b2
FischettiFrance <fischetti.francesco@iisgalvanimi.edu.it>
2019-12-03 12:51:37 +0000

    voto 8

--- EserLego/app.js
+++ EserLego/app.js
@@ -7,10 +7,15 @@ app.use(express.static(__dirname + '/public')); // Dico ad express dove recupera
 
 app.get('/', (req, res) => {
   res.render('index', {
-    castelli: castelli.castles //Passa il vettore profiles alla pagina index.pug
+    castellis: castelli.castles //Passa il vettore profiles alla pagina index.pug
   });
-});
+}); 
+app.get('/istruzioni',(req,res) => {
+  let castle = castelli.castles.find(castello => castello.SetNumber==req.query.id);
+console.log(castle);
+  res.render('istruzioni',{castle});
 
+})
 app.listen(3000, function () {
  console.log('Example app listening on port 3000!');
 });
